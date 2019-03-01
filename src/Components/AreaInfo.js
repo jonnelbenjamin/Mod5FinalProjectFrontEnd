8f52eb0597ac9ad783370f72654598f105a6df1a
github username <github email address>
2019-03-01 14:13:18 -0500

    did a lot for the areainfo component

--- src/Components/AreaInfo.js
+++ src/Components/AreaInfo.js
@@ -7,15 +7,21 @@ import { connect } from 'react-redux'
 class AreaInfo extends React.Component {
 
   state = {
-    areaInfo: {}
+    areaInfo: {},
+    locationInfo: {},
+    orgInfo: {}
+
   }
+
 
   findAreaInfo = () => {
     let locationName = this.props.selectedPlace.name
     this.props.disasters.find((disaster) => {
       if (disaster.location.name === locationName) {
         this.setState({
-          areaInfo: disaster
+          areaInfo: disaster,
+          locationInfo: disaster.location,
+          orgInfo: disaster.organizations[0]
         })
       }
     })
@@ -29,8 +35,17 @@ class AreaInfo extends React.Component {
 
   render(){
     return (
-
-      <div>"I'm the area info!"</div>
+      <div>
+      <h1>{this.state.areaInfo.active == false ? this.state.areaInfo.description + " hit the area" : this.state.areaInfo.description + " in the area"}</h1>
+      <h2>Location Information:</h2>
+      <h3>Name: {this.state.locationInfo.name} <button>Follow</button></h3>
+      <h3>Country GDP: ${this.state.locationInfo.country_gdp}</h3>
+      <h3>Description: {this.state.locationInfo.description}</h3>
+      <h2>Organizations Information</h2>
+      <h3>Name: {this.state.orgInfo.name} <button>Follow</button></h3>
+      <h3>Financial Need: ${this.state.orgInfo.financial_need}</h3>
+      <h3>Description: {this.state.orgInfo.description}</h3>
+      </div>
     )
   }
 }
