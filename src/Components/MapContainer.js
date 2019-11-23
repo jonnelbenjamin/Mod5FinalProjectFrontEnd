6b9c9a1d63eb4a9f7a76ae7e787b0fd3cf2629a9
Jonnel Benjamin <jonnel.benjamin@gmail.com>
2019-11-23 16:26:14 -0600

    trying to simplify functional component

--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -11,75 +11,89 @@ import Iframe from 'react-iframe'
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import * as parkDate from "./data/skateboard-parks.json";
 
-export default function MapContainer() {
+ function MapContainer() {
 
-  useEffect(() => {
-    this.props.fetchingDisasters()
-    this.props.fetchingLocations()
-  }, []);
-
-
+  // useEffect(() => {
+  //   // this.props.fetchingDisasters()
+  //   // this.props.fetchingLocations()
+  // }, []);
 
 
 
-  const [viewport, setViewport] = useState({
-    latitude: 18.448347,
-    longitude: -20.793440,
-    width: "98.8vw",
-    height: "82.5vh",
-    zoom: 1.5
-  });
-  const [selectedPark, setSelectedPark] = useState(null);
 
-  useEffect(() => {
-    const listener = e => {
-      if (e.key === "Escape") {
-        setSelectedPark(null);
-      }
-    };
-    window.addEventListener("keydown", listener);
+  //
+  // const [viewport, setViewport] = useState({
+  //   latitude: 18.448347,
+  //   longitude: -20.793440,
+  //   width: "98.8vw",
+  //   height: "82.5vh",
+  //   zoom: 1.5
+  // });
+  // const [selectedPark, setSelectedPark] = useState(null);
+  //
+  // useEffect(() => {
+  //   const listener = e => {
+  //     if (e.key === "Escape") {
+  //       setSelectedPark(null);
+  //     }
+  //   };
+  //   window.addEventListener("keydown", listener);
+  //
+  //   return () => {
+  //     window.removeEventListener("keydown", listener);
+  //   };
+  // }, []);
 
-    return () => {
-      window.removeEventListener("keydown", listener);
-    };
-  }, []);
-
- const onMarkerClick = (props, marker, e) => {
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    })
-  }
+ // const onMarkerClick = (props, marker, e) => {
+ //    this.setState({
+ //      selectedPlace: props,
+ //      activeMarker: marker,
+ //      showingInfoWindow: true
+ //    })
+ //  }
 
   return (
     <div>
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken={APIkey}
-        mapStyle="mapbox://styles/mapbox/satellite-v9"
-        onViewportChange={viewport => {
-          setViewport(viewport);
-        }}
-      >
-
-
-
-        {this.props.disasters.map(disaster => (
-          <Marker
-            key={disaster.id}
-            active={disaster.active}
-            description={disaster.description}
-            name={disaster.location.name}
-            latitude={disaster['latitude']}
-            longitude={disaster['longitude']}
-            onClick={this.onMarkerClick}
-          >
-          {console.log('hit')}
-          </Marker>
-        ))}
-        </ReactMapGL>
-
+    <h1>The core leadership team of United Relief was birthed from
+    a group of individuals who have spent many past seasons living
+    and serving in different nations around the world. They gave their lives away to help
+    those who have lost everything in the midst of the chaos of this world. Young and passionate
+    friends became inspired visionaries to make a difference in the lives of those who can't make
+    a difference for themselves. Although tremendous progress has been made in the continuing
+    initiatives of United Relief, there is still much work to be done.
+    </h1>
+      // <ReactMapGL
+      //   // {...viewport}
+      //   // mapboxApiAccessToken={APIkey}
+      //   // mapStyle="mapbox://styles/mapbox/satellite-v9"
+      //   // onViewportChange={viewport => {
+      //   //   setViewport(viewport);
+      //   // }}
+      // >
+      //   // {this.props.disasters.map(disaster => (
+      //   //   <Marker
+      //   //     key={disaster.id}
+      //   //     active={disaster.active}
+      //   //     description={disaster.description}
+      //   //     name={disaster.location.name}
+      //   //     latitude={disaster['latitude']}
+      //   //     longitude={disaster['longitude']}
+      //   //     onClick={this.onMarkerClick}
+      //   //   >
+      //   //   {console.log('hit')}
+      //   //   </Marker>
+      //   // ))}
+      //   </ReactMapGL>
     </div>
-  )
+  );
 }
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     fetchingDisasters: () => {dispatch(fetchingDisasters())},
+//     fetchingLocations: () => {dispatch(fetchingLocations())}
+//   }
+// }
+
+// export default connect(mapDispatchToProps)(MapContainer);
+export default MapContainer;
